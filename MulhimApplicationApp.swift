48fcd8591e0760a5cd50002ee36421f36d4d347e
leen449 <leenmeakel@gmail.com>
2025-08-03 11:43:29 +0300

    Add files via upload

---
+++ MulhimApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MulhimApplicationApp.swift
+//  MulhimApplication
+//
+//  Created by leen binmueqal on 08/02/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct MulhimApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
