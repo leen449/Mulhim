ed9f4bbd655d1fff2ad9eb05704ca9d529eece4a
leen449 <leenmeakel@gmail.com>
2025-08-05 21:14:01 +0300

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,11 +1,19 @@
+//
+//  ContentView.swift
+//  Mulhim2
+//
+//  Created by leen binmueqal on 10/02/1447 AH.
+//
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        SplashScreen()
+       OnboardingCoordinator()
     }
 }
 
 #Preview {
     ContentView()
 }
+
